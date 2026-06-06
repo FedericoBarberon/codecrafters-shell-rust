6016647276f1ec362752dd3939e615e700bdac81
FedericoBarberon <jfedericobarberonj@gmail.com>
2026-06-06 00:52:54 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut buf = String::new();
-    io::stdin()
-        .read_line(&mut buf)
-        .expect("failed to read command");
+        let mut buf = String::new();
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("failed to read command");
 
-    let command = buf.trim();
-    println!("{command}: command not found");
+        let command = buf.trim();
+        println!("{command}: command not found");
+    }
 }
